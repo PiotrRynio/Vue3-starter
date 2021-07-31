f7dc4f3201bd30085366d70570866d54dd9a6a62
PiotrWR <pwrynio@gmail.com>
2021-07-31 19:54:04 +0200

    Add two main sections

---
+++ src/components/organisms/AppBody/AppBody.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>AppBody</div>
+</template>
+
+<script>
+export default {
+  name: 'AppBody',
+};
+</script>
+
+<style scoped></style>
--- src/components/pages/RickAndMortyCharacters/RickAndMortyCharacters.vue
+++ src/components/pages/RickAndMortyCharacters/RickAndMortyCharacters.vue
@@ -1,10 +1,20 @@
 <template>
+  <AppHeader />
+  <AppBody />
+
   <div>example text</div>
 </template>
 
 <script>
+import AppHeader from '@/components/organisms/AppHeader/AppHeader';
+import AppBody from '@/components/organisms/AppBody/AppBody';
+
 export default {
   name: 'RickAndMortyCharacters',
+  components: {
+    AppHeader,
+    AppBody,
+  },
 };
 </script>
 
